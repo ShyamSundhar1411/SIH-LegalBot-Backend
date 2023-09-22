5838b2081554183d3945746f9527bc246ef736bb
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-09-22 22:09:09 +0530

    Db Schema

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(100),nullable=False)
+    password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Boolean,default = False)
+    messages = db.relationship('Message', backref='user', lazy=True)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_is_active(self):
+        return self.is_active
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    def is_admin(self):
+        return self.admin
+    def get_id(self):
+        return self.id
+    def __repr__(self):
+        return self.username
+class Message(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    is_user = db.Column(db.Boolean,default = True)
+    timestamp = db.Column(db.DateTime,default=datetime.utcnow)
+
+    
